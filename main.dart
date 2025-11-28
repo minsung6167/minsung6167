c58ff9c070aa63c3fb7b8d9f4975c31326f18838
minsung6167 <minsung1408@naver.com>
2025-11-28 20:33:00 +0900

    Add files via upload
    sample

---
+++ main.dart
@@ -0,0 +1,35 @@
+import 'package:shared_preferences/shared_preferences.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+import 'app.dart';
+import 'firebase_options.dart';
+
+late SharedPreferences prefs;
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  prefs = await SharedPreferences.getInstance();
+  await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return GetMaterialApp(
+      title: '당근마켓 클론 코딩',
+      initialRoute: '/',
+      theme: ThemeData(
+        appBarTheme: const AppBarTheme(
+          elevation: 0,
+          color: Color(0xff212123),
+          titleTextStyle: TextStyle(color: Colors.white),
+        ),
+        scaffoldBackgroundColor: const Color(0xff212123),
+      ),
+      getPages: [GetPage(name: '/', page: () => const App())],
+    );
+  }
+}
